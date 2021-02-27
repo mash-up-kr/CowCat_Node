5697300ded6fe44798e5e52803af734fb585f5bd
nari1021 <y.shhh000@gmail.com>
2021-02-28 03:57:41 +0900

    feat : CounselingQuestion models 수정

--- src/models/CounselingQuestion.js
+++ src/models/CounselingQuestion.js
@@ -2,44 +2,61 @@ import {Sequelize} from 'sequelize';
 const {Model, DataTypes} = Sequelize;
 
 export default (sequelize) => {
-  class CounselingQuestion extends Model {
-    static associate(models) {
-      this.belongsTo(models.User, {
-        foreignKey: 'userId',
-        as: 'User',
-      });
+    class CounselingQuestion extends Model {
+      static associate(models) {
+        this.hasOne(models.Category, {
+            foreignKey: 'categoryId ', 
+            as: 'Category',
+        });
 
-      //   this.belongsTo(models.Category, {
-      //       foreignKey: 'categoryId',
-      //       as: 'Category',
-      //   });
+        this.hasOne(models.Emotion, {
+            foreignKey: 'emotionId ',
+            as: "Emotion",
+        });
 
-    //   this.belongsTo(models.Emotion, {
-    //       foreignKey: "emotionId",
-    //       as: "Emotion",
-    //   })
+        this.belongsTo(models.User, {
+            foreignKey: 'userId',
+            as: "User",
+        });
+      }
     }
-  }
+    
+    CounselingQuestion.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        tittle: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+        content:{
+            type: DataTypes.STRING(200),
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+        deleted_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        }
+    }, {
+        sequelize,
+        timestamps: false,
+        tableName: 'counselingquestions',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    });
 
-  CounselingQuestion.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-      unique: true,
-    },
-  }, {
-    sequelize,
-    tableName: 'users',
-    charset: 'utf8',
-    timestamps: true,
-    underscored: true,
-  });
-
-  return CounselingQuestion;
+    return CounselingQuestion;
 };
