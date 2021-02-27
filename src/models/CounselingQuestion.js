5b6c9c47b502547d6729780caa639151b76e41f0
Seonjae Kim <rhj4862@gmail.com>
2021-02-28 07:54:05 +0900

    chore: babelLint 와 collate 를 추가합니다. (#16)
    * feat: ioredis, jsonwebtoekn 추가합니다.
    
    * feat: babel-lint added
    
    * fix: model collate option added
    
    * fix: package-lock conflicts

--- src/models/CounselingQuestion.js
+++ src/models/CounselingQuestion.js
@@ -36,7 +36,8 @@ export default (sequelize) => {
   }, {
     sequelize,
     tableName: 'users',
-    charset: 'utf8',
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_bin',
     timestamps: true,
     underscored: true,
   });
