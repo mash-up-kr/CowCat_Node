6a2dd83e96f46553dad71b4297c2721a59da7cf4
kor-Chipmunk <rhj4862@gmail.com>
2021-03-01 14:27:25 +0900

    fix: 서버 실행 타임아웃을 늘림

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -5,4 +5,4 @@ echo '======================'
 
 cd /home/ec2-user/app/dist/CowCat_Node/
 source /home/ec2-user/.bash_profile
-npx pm2 reload ecosystem.config.cjs --env production &
+npx pm2 reload ecosystem.config.cjs --env production
