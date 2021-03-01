478cdc4fb03baad9451670dbe9348f45b11c4691
kor-Chipmunk <rhj4862@gmail.com>
2021-03-01 13:05:29 +0900

    chore: 스크립트 및 훅 설정파일 추가

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+# scripts/install_dependencies
+echo '============================'
+echo 'Running install_dependencies'
+echo '============================'
+
+cd /home/ec2-user/app/dist/CowCat_Node/
+source /home/ec2-user/.bash_profile
+npm install
