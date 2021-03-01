74c67b79860425abd138abf475f1e38a3bb5e534
kor-Chipmunk <rhj4862@gmail.com>
2021-03-01 14:00:35 +0900

    fix: ecosystem.config CJS 확장자로 수정

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: './src/index.js',
+      instances: 0,
+      exec_mode: 'cluster',
+      wait_ready: true,
+      listen_timeout: 50000,
+      kill_timeout: 5000,
+      env: {
+        'NODE_ENV': 'develop',
+      },
+      env_production: {
+        'NODE_ENV': 'production',
+      },
+    },
+  ],
+};
