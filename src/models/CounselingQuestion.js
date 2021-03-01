9760a18332ce599467f943f2ae7a6c4a40a40a73
kor-Chipmunk <rhj4862@gmail.com>
2021-03-01 12:07:16 +0900

    fix: ESModule sequelize 오류 해결

--- src/models/CounselingQuestion.js
+++ src/models/CounselingQuestion.js
@@ -1,46 +1,46 @@
-import {Sequelize} from 'sequelize';
+import db from './sequelize.js';
+
+const {sequelize, Sequelize} = db;
 const {Model, DataTypes} = Sequelize;
 
-export default (sequelize) => {
-  class CounselingQuestion extends Model {
-    static associate(models) {
-      this.belongsTo(models.User, {
-        foreignKey: 'userId',
-        as: 'User',
-      });
+class CounselingQuestion extends Model {
+  static associate(models) {
+    this.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'User',
+    });
 
-      //   this.belongsTo(models.Category, {
-      //       foreignKey: 'categoryId',
-      //       as: 'Category',
-      //   });
+    // this.belongsTo(models.Category, {
+    //   foreignKey: 'categoryId',
+    //   as: 'Category',
+    // });
 
-    //   this.belongsTo(models.Emotion, {
-    //       foreignKey: "emotionId",
-    //       as: "Emotion",
-    //   })
-    }
+    // this.belongsTo(models.Emotion, {
+    //   foreignKey: 'emotionId',
+    //   as: 'Emotion',
+    // });
   }
+}
 
-  CounselingQuestion.init({
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING(20),
-      allowNull: false,
-      unique: true,
-    },
-  }, {
-    sequelize,
-    tableName: 'counseling_questions',
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_bin',
-    timestamps: true,
-    underscored: true,
-  });
+CounselingQuestion.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING(20),
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  sequelize,
+  tableName: 'counseling_questions',
+  charset: 'utf8mb4',
+  collate: 'utf8mb4_bin',
+  timestamps: true,
+  underscored: true,
+});
 
-  return CounselingQuestion;
-};
+export default CounselingQuestion;
