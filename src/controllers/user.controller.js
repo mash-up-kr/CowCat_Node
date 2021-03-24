cefe63d29be4ae79bcd63f25b086421dcd6cc2f5
kor-Chipmunk <rhj4862@gmail.com>
2021-03-24 10:31:33 +0900

    feat: 회원가입과 JWT 발급 기능을 추가합니다.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,39 @@
-// import countryService from '../services/user.service.js';
-// import {Success, Failure} from '../utils/response.js';
-// import {redisDefault, getAsyncReadonly} from '../redis.js';
+import userService from '../services/user.service.js';
+import {Success, Failure} from '../utils/response.js';
+import {redisDefault, getAsyncReadonly} from '../redis.js';
+
+import jsonwebtoken from '../auth/jsonwebtoken.js';
 
 export const postSignUp = async (req, res, next) => {
+  const {
+    nickname,
+    birthdayYear,
+    gender,
+    imageUrl,
+  } = req.body;
 
-};
+  try {
+    const user = await userService.signUp(
+        req.snsId,
+        req.snsType,
+        nickname,
+        birthdayYear,
+        gender,
+        imageUrl,
+    );
 
-export const postSignIn = async (req, res, next) => {
+    // 생일년도-01-01 으로 바꿔주기 위해 Json 결과를 조작합니다.
+    const jsonResult = Success(user);
+    jsonResult.data.dataValues.birthday = `${birthdayYear}-01-01`;
 
+    // 토큰 정보를 같이 넣어줍니다.
+    const tokens = await jsonwebtoken.createToken(req.snsId, req.snsType);
+    jsonResult.data.dataValues.token = tokens;
+
+    return res.status(200).json(jsonResult);
+  } catch (error) {
+    return res.status(200).json(Failure(error));
+  }
 };
 
 export const getMyProfile = async (req, res, next) => {
@@ -15,7 +41,14 @@ export const getMyProfile = async (req, res, next) => {
 };
 
 export const putMyProfile = async (req, res, next) => {
+  const {
+    nickname,
+    gender,
+    birthdayYear,
+    location,
+  } = req.body;
 
+  return Success(req.body);
 };
 
 export const postRefreshToken = async (req, res, next) => {
@@ -28,7 +61,6 @@ export const postCheckNickname = async (req, res, next) => {
 
 export default {
   postSignUp,
-  postSignIn,
   getMyProfile,
   putMyProfile,
   postRefreshToken,
