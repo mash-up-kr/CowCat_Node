8ff2e5dd96fc177ec09a9314a42cc207ab899911
batboy118 <118jjh@naver.com>
2021-04-10 14:19:46 +0900

    style: eslint 적용

--- src/models/CounselingComment.js
+++ src/models/CounselingComment.js
@@ -1,7 +1,7 @@
 import db from './sequelize.js';
 
-const { sequelize, Sequelize } = db;
-const { Model, DataTypes } = Sequelize;
+const {sequelize, Sequelize} = db;
+const {Model, DataTypes} = Sequelize;
 
 class CounselingComment extends Model {
   static associate(models) {
@@ -19,27 +19,27 @@ class CounselingComment extends Model {
 }
 
 CounselingComment.init(
-  {
-    id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      content: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
     },
-    content: {
-      type: DataTypes.STRING(200),
-      allowNull: false,
+    {
+      sequelize,
+      tableName: 'counseling_comments',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_bin',
+      timestamps: true,
+      paranoid: true,
+      underscored: true,
     },
-  },
-  {
-    sequelize,
-    tableName: 'counseling_comments',
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_bin',
-    timestamps: true,
-    paranoid: true,
-    underscored: true,
-  }
 );
 
 export default CounselingComment;
--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 import db from './sequelize.js';
 
-const { sequelize, Sequelize } = db;
-const { Model, DataTypes } = Sequelize;
+const {sequelize, Sequelize} = db;
+const {Model, DataTypes} = Sequelize;
 
 class User extends Model {
   static associate(models) {
@@ -25,53 +25,53 @@ class User extends Model {
 }
 
 User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    snsId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    snsType: {
-      type: DataTypes.ENUM(['kakao']),
-      allowNull: false,
-    },
-    nickname: {
-      type: DataTypes.STRING(10),
-      allowNull: true,
-    },
-    birthday: {
-      type: DataTypes.DATE,
-      allowNull: true,
-    },
-    gender: {
-      type: DataTypes.ENUM(['F', 'M']),
-      defaultValue: 'F',
-      allowNull: true,
-    },
-    imageUrl: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-    },
-  },
-  {
-    sequelize,
-    tableName: 'users',
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_bin',
-    timestamps: true,
-    underscored: true,
-    indexes: [
-      {
-        fields: ['image_url'],
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      { unique: true, fields: ['nickname'] },
-    ],
-  }
+      snsId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      snsType: {
+        type: DataTypes.ENUM(['kakao']),
+        allowNull: false,
+      },
+      nickname: {
+        type: DataTypes.STRING(10),
+        allowNull: true,
+      },
+      birthday: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      gender: {
+        type: DataTypes.ENUM(['F', 'M']),
+        defaultValue: 'F',
+        allowNull: true,
+      },
+      imageUrl: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'users',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_bin',
+      timestamps: true,
+      underscored: true,
+      indexes: [
+        {
+          fields: ['image_url'],
+        },
+        {unique: true, fields: ['nickname']},
+      ],
+    },
 );
 
 export default User;
