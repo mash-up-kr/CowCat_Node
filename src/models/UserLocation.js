f283d7bf0d866e683334f000dbe306cd1798daf4
batboy118 <118jjh@naver.com>
2021-04-10 21:06:27 +0900

    feat: 모델 카테고리, 감정 컬럼 추가 및 컨벤션 수정

--- src/models/UserLocation.js
+++ src/models/UserLocation.js
@@ -7,33 +7,36 @@ class UserLocation extends Model {
   static associate(models) {
     this.belongsTo(models.User, {
       foreignKey: 'userId',
-      as: 'User',
+      as: 'user',
     });
   }
 }
 
-UserLocation.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  latitude: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  longitude: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-}, {
-  sequelize,
-  tableName: 'user_locations',
-  charset: 'utf8mb4',
-  collate: 'utf8mb4_bin',
-  timestamps: true,
-  underscored: true,
-});
+UserLocation.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      latitude: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+      longitude: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'user_locations',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_bin',
+      timestamps: true,
+      underscored: true,
+    },
+);
 
 export default UserLocation;
